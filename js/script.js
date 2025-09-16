abf113a06fc3c1faf7239b379afa108ad0232fbe
goCatrilef <go.catrilef@duocuc.cl>
2025-09-16 05:01:35 -0300

    Pagina principal corregida

--- js/script.js
+++ js/script.js
@@ -4,27 +4,45 @@ let product = []
 
 
 function mostrarProductos({ id, imagen, nombre, precio, descripcion } = product) {
-    return ` 
+    return `    
+            <div class="col-md-3 mb-4">
+                <div class="card h-100">
                     <img src="assets/${imagen}" class="card-img-top" alt="${nombre}">
                     <div class="card-body d-flex flex-column">
-                        <h5 class="card-title">${nombre}:</h5>
+                        <h5 class="card-title text-center">${nombre}:</h5>
                         <p class="card-text">${descripcion}</p>
                         <div class="mt-auto">
                             <span class="text-ch--pink h5">${precio}</span>
-                            <button id="${id}" class="btn btn-ch--pink btn-sm float-end">Agregar al carrito</button>
+                            <a href="./productos.html" class="btn btn-ch--pink btn-sm float-end">Ver Producto</a>
                         </div>
                     </div>
-
-
+                </div>
+            </div>
             `;
 }
 
+// function cargarProductos(array) {
+//     main.innerHTML = '';
+//     if (array.length > 0) {
+//         array.forEach((product) => {
+//             main.innerHTML += mostrarProductos(product);
+//         });
+//     } else {
+//         main.innerHTML = `<h3 class="text-center">No se encontraron productos</h3>`;
+//     }
+// }
+
 function cargarProductos(array) {
     main.innerHTML = '';
-    if (array.length > 0) {
-        array.forEach((product) => {
-            main.innerHTML += mostrarProductos(product);
-        });
+    const productosPorPagina = 16;
+    const productosMostrados = array.slice(0, productosPorPagina);
+
+    if (productosMostrados.length > 0) {
+        for (let i = 0; i < productosMostrados.length; i += 4) {
+            main.innerHTML += `<div class="row">` +
+                productosMostrados.slice(i, i + 4).map(mostrarProductos).join('') +
+                `</div>`;
+        }
     } else {
         main.innerHTML = `<h3 class="text-center">No se encontraron productos</h3>`;
     }
